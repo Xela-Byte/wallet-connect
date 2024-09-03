313e209679e0e39c8592ab76ad128879f46abeae
Xela-Byte <xelaoladipupo@gmail.com>
2024-09-03 12:27:26 +0100

    Updated dependices

---
+++ src/wagmi.ts
@@ -0,0 +1,24 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'bdfce4f4a58463330629fcd8f4d69b04',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  ssr: true,
+});
+
